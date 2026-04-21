ba4b8da027b6c6e59be6fa20b9479f81374fcd84
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-21 23:39:46 +0600

    added button and add bg color purple

--- js/event.js
+++ js/event.js
@@ -5,3 +5,16 @@ function makeYellow() {
 function makeRed() {
   document.body.style.backgroundColor = 'Red';
 }
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+//  console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeblue() {
+  document.body.style.backgroundColor = 'blue';
+};
+
+const btnMakePurple = document.getElementById('btn-make-purple');
+// console.log(btnMakePurple)
+btnMakePurple.onclick = makePurple;
+function makePurple() {
+  document.body.style.backgroundColor = 'purple';
+}
