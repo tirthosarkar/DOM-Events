a0ab7b4d29f9bf575738c2d3564d8ac991d283d4
tirthosarkar <tirthosarkar1@gmail.com>
2026-05-01 16:00:01 +0600

    added page-title and change text

--- js/handler.js
+++ js/handler.js
@@ -1,1 +1,8 @@
-console.log('handler file added')
+document
+  .getElementById('btn-update-title')
+  .addEventListener('click', function () {
+    // console.log('btn clicked');
+    const pageTitleElement = document.getElementById('page-title');
+    console.log(pageTitleElement);
+    pageTitleElement.innerText = 'Updated page title text';
+  });
